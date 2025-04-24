8ec2e8e20ddbaf2eadd9dd2a787bb600d3419218
Matheus440 <41317968+Matheus440@users.noreply.github.com>
2025-04-23 21:57:59 -0300

    Correção de bugs, ajustes e melhorias para a experiência do cliente.

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 let dolar = 5.8
 
 let usdInput = document.querySelector("#usd")
-let brlInput = document.querySelector("#brl") 
+let brlInput = document.querySelector("#brl")
 
 usdInput.addEventListener("keyup", () => {
-    usdInput.value = formatCurrency(usdInput.value)
-
+    convert("usd-to-brl")
 })
 
 brlInput.addEventListener("keyup", () => {
-    brlInput.format = formatCurrency(brlInput.value)
-    
+    convert("brl-to-usd")
 })
 
-usdInput.value = "1000"
+
+usdInput.value = "1000,00"
 convert("usd-to-brl")
 
-//function to convert the values
+//funcao para formatar o valor em reais
 function formatCurrency(value) {
-    let fixedValue = fixValue(value) //ajust value 
+    let fixedValue = fixValue(value)  
     let options = {
         useGrouping: false,
         minimumFractionDigits: 2
     }
     let formartter = new Intl.NumberFormat("pt-BR", options)
-    return formartter.format(fixValue)    
+    return formartter.format(fixedValue)
 }
 
+
 function fixValue(value) {
-    let fixValue = value.replace(",", ".")
-    let floatValue = parseFloat(fixValue)
-    if (floatValue == NaN) {
+    let fixedValue = value.replace(",", ".")
+    let floatValue = parseFloat(fixedValue)
+    if (isNaN(floatValue)) {
         floatValue = 0
     }
     return floatValue
@@ -40,14 +40,16 @@ function fixValue(value) {
 
 function convert(type) {
     if (type == "usd-to-brl") {
-
-
-
+        let fixedValue = fixValue(usdInput.value)
+        let result = fixedValue * dolar
+        result = result.toFixed(2)
+         brlInput.value = formatCurrency(result)
     }
 
     if (type == "brl-to-usd") {
-    
-    
-    
-    }   
+        let fixedValue = fixValue(brlInput.value)
+        let result = fixedValue / dolar
+        result = result.toFixed(2)
+        usdInput.value = formatCurrency(result)
+    }
 }
